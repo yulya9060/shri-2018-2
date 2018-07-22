3648d73090cc047445c443642335136787a0d558
Julia <yulya_9060@mail.ru>
2018-07-22 12:19:46 +0300

    изменила верстку карточек

--- source/pages/main/main.js
+++ source/pages/main/main.js
@@ -1,20 +1,2 @@
 import './main.scss';
 import '../../fonts/fonts.scss';
-
-var boxs =document.querySelectorAll('.scenario__title');
-for (var i = 0; i <boxs.length; i++) {
-  var parent=boxs[i].parentElement;
-  var  text = boxs[i].innerHTML;
-   var clone = document.createElement('div');
-  clone.style.position = 'absolute';
-clone.style.visibility = 'hidden';
-clone.style.width = boxs[i].clientWidth + 'px';
-clone.innerHTML = text;
-parent.appendChild(clone);
-var l = text.length - 1;
-  for (; l >= 0 && clone.clientHeight > boxs[i].clientHeight; --l) {
-    clone.innerHTML = text.substring(0, l) + '...';
-}
-  boxs[i].innerHTML = clone.innerHTML;
-  console.log( clone.clientHeight,boxs[i].clientHeight);
-}
