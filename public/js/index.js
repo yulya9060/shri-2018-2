5596a2a76a7579ab0c5b940260d734008eea058b
Julia <yulya_9060@mail.ru>
2018-07-24 21:15:24 +0300

    обновила папку public

---
+++ public/js/index.js
@@ -0,0 +1,94 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index_scss__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__index_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__index_scss__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__fonts_fonts_scss__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__fonts_fonts_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__fonts_fonts_scss__);
+
+
+
+
+
+const cardTemperatures=document.querySelectorAll(".card-temperature");
+const cardLights=document.querySelectorAll(".card-light");
+const cardFloors=document.querySelectorAll(".card-floor");
+const floorSlider=document.getElementById('child');
+const btnCancels=document.querySelectorAll('.popap-btn-cancel');
+
+    Array.prototype.filter.call(btnCancels, function (btn) {
+        btn.addEventListener('click', function () {
+            let parent=this.parentNode.parentNode.parentNode;
+            if (this.closest('.popap__temperature')){
+                parent.classList.remove('popap__temperature');
+            }
+            else if (this.closest('.popap__light')){
+                parent.classList.remove('popap__light');
+            }
+            else if (this.closest('.popap__floor')){
+                parent.classList.remove('popap__floor');
+            }
+        });
+    });
+    Array.prototype.filter.call(cardTemperatures, function (cardTemperature) {
+        cardTemperature.addEventListener('click', function () {
+            document.getElementById('popap-temperature').classList.add('popap__temperature');
+      });
+    });
+
+    Array.prototype.filter.call(cardLights, function (cardLight) {
+        cardLight.addEventListener('click', function () {
+            document.getElementById('popap-light').classList.add('popap__light');
+      });
+    });
+
+    Array.prototype.filter.call(cardFloors, function (cardFloor) {
+        cardFloor.addEventListener('click', function () {
+            document.getElementById('popap-floor').classList.add('popap__floor');
+      });
+    });
+
+
+    floorSlider.addEventListener('click', function(e) {
+        let arrow=e.target;
+        let circle=360/24;
+        let currentGetOffset=getComputedStyle(e.target).offsetDistance;
+        let currentTemp=arrow.nextElementSibling;
+        let innerTemp=parseInt(currentTemp.innerHTML);
+        console.log('etarget',e.target,currentTemp);
+        if (parseInt(currentGetOffset,10)>100){
+          currentGetOffset=0;
+        }
+        if (parseInt(innerTemp,10)>=36){
+          innerTemp=-6;
+        }
+        console.log('currentGetOffset',currentGetOffset,`${circle}%`);
+        arrow.style.offsetDistance= currentGetOffset;
+        let newOffset=parseInt(currentGetOffset,10)+parseInt(circle,10);
+        arrow.style.offsetDistance= `${newOffset}%`;
+        console.log('newOffset', `${newOffset}`);
+        currentGetOffset=getComputedStyle(e.target).offsetDistance;
+        innerTemp+=6;
+        currentTemp.innerHTML=`+${innerTemp}`;
+        arrow.style.duration= '2000ms';
+        arrow.style.easing='linear';
+      })
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+],[0]);
